a4aeb8e7869b870578943346f16c6a9bab0ac647
Rodrigo Ganância <rodrigo.ganancia@tecnico.ulisboa.pt>
2024-07-18 21:54:45 +0100

    Fixed PageHeader argument

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         />
       </div>
       <div>
-        <PageHeader>Click me</PageHeader>
+        <PageHeader></PageHeader>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
