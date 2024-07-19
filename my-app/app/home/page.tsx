9c23920583d7bd417d634855cefc90d9c2a09200
Rodrigo Ganância <rodrigo.ganancia@tecnico.ulisboa.pt>
2024-07-19 11:54:01 +0100

    Updated navbar

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-t from-[#f2aaaa] to-[#a6c1ee] h-screen">
       <MenuBar></MenuBar> 
+      <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     </div>
   );
 }
--- my-app/components/menu-bar.tsx
+++ my-app/components/menu-bar.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import React from 'react';
+import { useState } from 'react';
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 import Link from "next/link";
-import { AlignJustify } from 'lucide-react';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -18,10 +18,14 @@ import '@/app/styles/menu-bar.css';
 
 export default function MenuBar() {
 
-  const handleClick = () => {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks?.classList.toggle('top-[12%]');
-  };
+  const navLinks = [
+    {name:"Página Inicial", link: "/home"},
+    {name:"Notícias", link: "/noticias"},
+    {name:"Contactos", link: "/contactos"},
+    {name:"Sócios", link: "/socios"},  
+  ]
+
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="bg-white">
@@ -34,26 +38,23 @@ export default function MenuBar() {
             height={100}
           />
         </div>
-        <div className="nav-links md:static absolute bg-white md:min-h-fit min-h-[60-vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-8">
+        <div className={`md:static absolute bg-white md:min-h-fit min-h-[60-vh]
+          left-0 md:w-auto w-full flex items-center px-8 ${open ? "top-[12%]" : "top-[-100%]"}`}>
           <ul className="flex md:flex-row flex-col items-center gap-[4vw]">
-            <li className="hover:text-gray-500">
-              <Link href="/noticias">Notícias</Link>
-            </li>
-            <li className="hover:text-gray-500">
-              <Link href="/sobre">Sócios</Link>
-            </li>
-            <li className="hover:text-gray-500">
-              <Link href="/sobre">Sobre Nós</Link>
-            </li>
-            <li className="hover:text-gray-500 pb-4 md:pb-0">
-              <Link href="/contactos">Contactos</Link>
-            </li>
+            {
+              navLinks.map((link) => (
+                <li className="hover:text-gray-500 pb-4 md:pb-0">
+                  <Link href={link.link}>{link.name}</Link>
+                </li>
+              ))
+            }
           </ul>
         </div>
-
         <div className="flex items-center gap-6">
-          <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Sign in</button>
-          <AlignJustify className="cursor-pointer text-3xl md:hidden" onClick={handleClick}/>
+          <Button className="">Sign in</Button>
+          <div className="text-3xl cursor-pointer md:hidden" onClick={() => setOpen(!open)}>
+            <ion-icon name={open ? "close" : "menu"}></ion-icon>
+          </div>
         </div>
       </nav>
     </div>
