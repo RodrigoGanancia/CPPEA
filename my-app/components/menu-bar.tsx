e12f60427da2e56fc53bb81542215732ab662e4b
Rodrigo Ganância <rodrigo.ganancia@tecnico.ulisboa.pt>
2024-07-19 13:45:06 +0100

    Added react icons

--- my-app/components/menu-bar.tsx
+++ my-app/components/menu-bar.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import '@/app/styles/menu-bar.css';
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const MenuBar = () => {
   const navLinks = [
@@ -37,13 +39,12 @@ const MenuBar = () => {
           </ul>
         </div>
         <div className="flex items-center gap-8">
-          <div className="flex items-center gap-3 text-4xl cursor-pointer">
-            <ion-icon name="logo-instagram"></ion-icon>
-            <ion-icon name="logo-facebook"></ion-icon>
-            <ion-icon name="mail"></ion-icon>
+          <div className="flex items-center gap-4 text-4xl cursor-pointer">
+            <FaInstagram />
+            <FaFacebook />
           </div>
           <div className="text-3xl cursor-pointer md:hidden" onClick={() => setOpen(!open)}>
-            <ion-icon name={open ? "close" : "menu"}></ion-icon>
+            {open ? <IoClose /> : <IoMenu />}
           </div>
         </div>
       </nav>
